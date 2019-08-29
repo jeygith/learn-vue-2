a39da23f522e2eccf91564e777487d428f2d6f44
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-29 12:06:46 +0300

    Add custom modal component in vue

---
+++ 10-modal-component/js/main.js
@@ -0,0 +1,33 @@
+Vue.component('modal', {
+    props: ['title', 'body'],
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+    template: ` <div class="modal is-active">
+        <div class="modal-background"></div>
+
+        <div class="modal-content">
+            <!-- Any other Bulma elements you want -->
+            <div class="box">
+                <slot></slot>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+`,
+    methods: {
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+})
+
+new Vue({
+    el: '#root',
+
+    data: {
+        showModal: false
+    }
+});
