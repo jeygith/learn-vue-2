59374f0d41f7444875247bc47fbdb4abc2130a1d
Jeff Githiri <geoffrey.githiri@andela.com>
2019-10-22 11:36:35 +0300

    17-hot-module-replacement

---
+++ 17-Hot-module-replacement/lesson/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+
+    import Counter from "./components/Counter";
+
+    export default {
+        name: 'app',
+        components: {
+            Counter
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 17-Hot-module-replacement/lesson/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>The Count: {{count}}</h2>
+
+    <p>
+      <button @click="count +=10">Increment</button>
+    </p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+      data(){
+          return {
+              count : 0
+          }
+      }
+  }
+</script>
